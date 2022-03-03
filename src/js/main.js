ff56222e3a01b178cc882a1fce860470ae557d00
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-03-04 03:02:56 +0600

    Update Design & Update Temp, City Name, Icon

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+
+const api_key = `e50837b613fc8767d7de30f0b334d3c4`;
+
+
+document.getElementById('city-input-button').addEventListener('click', function(){
+    const searchTemp = ()=> {
+        const cityInput = document.getElementById('city-input');
+        const cityInputValue = cityInput.value;
+        cityInput.value = '';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputValue}&appid=${api_key}&units=metric`;
+        fetch(url)
+        .then(res => res.json())
+        .then(weatherData => DisplayWeatherData(weatherData));
+    };
+    const showWeather = (id, text) => {
+        const weatherShow = document.getElementById(id);
+        weatherShow.innerText = text;
+    };
+    searchTemp();
+   const DisplayWeatherData = (weatherData) => {
+       showWeather('weather-city', weatherData.name);
+       showWeather('weather-temp', weatherData.main.temp);
+       showWeather('weather-condition', weatherData.weather[0].main);
+    //    const weatherIcon = showWeather('weather-condition', weatherData.weather[0].icon);
+       const weatherIcon = document.getElementById('weather-icon');
+       weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`);
+    //    showWeather('weather-temp', weatherData.main.temp);
+       console.log(weatherData.weather[0]);
+   }
+// console.log('click');
+});
